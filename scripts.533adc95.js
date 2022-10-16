1fe819a43321bd1a2ab68f84b9d0d9d5aa70e8e3
pomazkovanton <vitrumbeta@gmail.com>
2022-10-16 20:39:31 +0800

    Updates

--- scripts.533adc95.js
+++ scripts.533adc95.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"g2Hq":[function(require,module,exports) {
 "use strict";var e=require("./variables"),n=u(require("./menu"));function u(e){return e&&e.__esModule?e:{default:e}}var r=function(){(0,n.default)(e.menu,e.iconOpenMenu,e.iconCloseMenu)};e.buttonMenu.addEventListener("click",r);
 },{"./variables":"EVjC","./menu":"moGU"}]},{},["g2Hq"], null)
-//# sourceMappingURL=/scripts.533adc95.js.map
+//# sourceMappingURL=lubimovka/scripts.533adc95.js.map
